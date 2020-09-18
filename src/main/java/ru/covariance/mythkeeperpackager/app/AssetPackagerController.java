412bec5c4cee2c20ac532498bf84c1d3f49c4773
CovarianceMomentum <covariancemomentum@gmail.com>
2020-09-19 02:52:48 +0300

    Asset Packager page done and functional

--- src/main/java/ru/covariance/mythkeeperpackager/app/AssetPackagerController.java
+++ src/main/java/ru/covariance/mythkeeperpackager/app/AssetPackagerController.java
@@ -1,11 +1,16 @@
 package ru.covariance.mythkeeperpackager.app;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.DirectoryChooser;
+import ru.covariance.mythkeeperpackager.packager.WonderdraftSymbolsPackager;
 
 public class AssetPackagerController {
 
@@ -16,18 +21,42 @@ public class AssetPackagerController {
   private URL location;
 
   @FXML
-  private Text assetPackagerText;
+  private TextField directoryPathTextField;
 
   @FXML
-  private Button assetPackagerButton;
+  private Button pathChooser;
 
   @FXML
-  void onAssetPackagerButtonClicked(MouseEvent event) {
-    assetPackagerText.setText("*boop*");
+  private Text resultMessage;
+
+  @FXML
+  private Button generateButton;
+
+  @FXML
+  void onGenerateButtonClicked(MouseEvent event) {
+    try {
+      resultMessage.setText("Generating...");
+      WonderdraftSymbolsPackager.packageDirectory(new File(directoryPathTextField.getText()));
+      resultMessage.setText("Done!");
+    } catch (IOException e) {
+      resultMessage.setText("Something went wrong.");
+    }
+  }
+
+  @FXML
+  void onPathChooserClicked(MouseEvent event) {
+    DirectoryChooser dirChooser = new DirectoryChooser();
+
+    File selectedDir = dirChooser.showDialog(null);
+
+    if (selectedDir != null) {
+      directoryPathTextField.setText(selectedDir.getAbsolutePath());
+    } else {
+      resultMessage.setText("Something went wrong.");
+    }
   }
 
   @FXML
   void initialize() {
-
   }
 }
